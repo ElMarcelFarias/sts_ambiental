33926e5a12aa3f8910a84f9480adb2d66e815ab4
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-08-12 16:59:47 -0300

    New: máscara de Telefone com jquery mask finalizada.

--- index.php
+++ index.php
@@ -651,7 +651,7 @@
                                                 <div class="form-group position-relative">
                                                     <label>Telefone <span class="text-danger">*</span></label> 
                                                     <svg aria-hidden="true" focusable="false" class="icon fea icon-sm icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"/></svg> 
-                                                    <input name="telefone" id="telefone" type="text" class="form-control pl-5" placeholder="Digite seu telefone : ">
+                                                    <input name="telefone" id="telefone" type="text" class="form-control pl-5 phone_with_ddd" placeholder="Digite seu telefone : ">
                                                 </div>
                                             </div>
 
@@ -818,7 +818,7 @@
 
     <!-- javascript -->
     <script src="js/jquery-3.5.1.min.js"></script>
-    
+    <script src="js/jquery.mask.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="js/jquery.easing.min.js"></script>
     <script src="js/scrollspy.min.js"></script>
@@ -850,6 +850,20 @@
         });
 
         //envio de email, validação via AJAX com Jquery
+        //$('.phone_with_ddd').mask('(00) 00000-0000');
+
+        $(document).ready(function(){
+            var SPMaskBehavior = function (val) {
+                return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+            },
+            spOptions = {
+                onKeyPress: function(val, e, field, options) {
+                    field.mask(SPMaskBehavior.apply({}, arguments), options);
+                }
+            };
+
+            $('#telefone').mask(SPMaskBehavior, spOptions);
+        });
 
         $(document).ready(function(){
         $("#contact-form").submit(function(e){
